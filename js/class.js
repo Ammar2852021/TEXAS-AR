3b1e7088f819dde76b1d12a74db5af539d3d84c2
Ammar Ahmed <AmmarAhmed132004@outlook.com>
2023-06-22 04:55:48 +0300

    ED

--- js/class.js
+++ js/class.js
@@ -8,7 +8,7 @@ getData(APIs.host + APIs.social).then((data) => {
   data["data"].forEach((e) => {
     if (protection_price) {
       if (e.key === "protection") {
-        protection_price.innerText = `$ ${e.value} /day`;
+        protection_price.innerText = `$ ${e.value} / لليوم`;
         pr_price = e.value * Number(localStorage.getItem("days"));
         _price = Number(total.innerText.match(/[0-9.,]+/)[0]);
       }
